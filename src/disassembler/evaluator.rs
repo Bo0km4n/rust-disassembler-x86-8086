64974ea265750acd9689581cbc58f342b255103e
Bo0km4n <pokopenkatuya@gmail.com>
2019-01-18 18:20:27 +0900

    tweak

--- src/disassembler/evaluator.rs
+++ src/disassembler/evaluator.rs
@@ -3,17 +3,16 @@ use super::reader::DisAssembler;
 use super::error;
 pub fn eval<T: DisAssembler>(state: &mut T) -> Result<String, error::InvalidOperationCode> {
     let cur_token = state.cur_token();
-    match cur_token {
+    let res = match cur_token {
         0x30 => xor::eval_xor_reg_mem(state),
+        0x31 => xor::eval_xor_reg_mem(state),
+        0x32 => xor::eval_xor_reg_mem(state),
+        0x33 => xor::eval_xor_reg_mem(state),
+        0x80 => xor::eval_xor_immediate_reg_mem(state),
+        0x81 => xor::eval_xor_immediate_reg_mem(state),
+        0x34 => xor::eval_xor_immediate_acm(state),
+        0x35 => xor::eval_xor_immediate_acm(state),
         _ => return Err(error::InvalidOperationCode{ op: cur_token })
-    }
-    // evaluator_fns.insert(0x30, xor::eval_xor_reg_mem);
-    // evaluator_fns.insert(0x31, xor::eval_xor_reg_mem);
-    // evaluator_fns.insert(0x32, xor::eval_xor_reg_mem);
-    // evaluator_fns.insert(0x33, xor::eval_xor_reg_mem);
-    // evaluator_fns.insert(0x80, xor::eval_xor_immediate_reg_mem);
-    // evaluator_fns.insert(0x81, xor::eval_xor_immediate_reg_mem);
-    // evaluator_fns.insert(0x34, xor::eval_xor_immediate_acm);
-    // evaluator_fns.insert(0x35, xor::eval_xor_immediate_acm);
-    return Ok("".to_string());
+    };
+    return Ok(res);
 }
--- src/disassembler/xor.rs
+++ src/disassembler/xor.rs
@@ -1,15 +1,18 @@
 use super::reader::DisAssembler;
-pub fn eval_xor_reg_mem<T: DisAssembler>(state: &mut T) {
+pub fn eval_xor_reg_mem<T: DisAssembler>(state: &mut T) -> String {
     println!("xor reg mem eval");
     state.dump_cur_token();
+    return "".to_string()
 }
 
-pub fn eval_xor_immediate_reg_mem<T: DisAssembler>(state: &mut T) {
+pub fn eval_xor_immediate_reg_mem<T: DisAssembler>(state: &mut T) -> String {
     println!("xor immediate reg mem");
     state.dump_cur_token();
+    return "".to_string()
 }
 
-pub fn eval_xor_immediate_acm<T: DisAssembler>(state: &mut T) {
+pub fn eval_xor_immediate_acm<T: DisAssembler>(state: &mut T) -> String {
     println!("xor immediate acm");
     state.dump_cur_token();
+    return "".to_string()
 }
